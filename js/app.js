1b8d3b0580262b04f29e3ac93df4136b93cdb021
WDsagor <sagarwpress@gmail.com>
2022-03-11 14:40:03 +0600

    read more complet

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+    return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -62,8 +62,7 @@ const createPost = (post) => {
                   <a
                     href="https://github.com/ProgrammingHero1"
                     target="_blank"
-                    class="post__avatar"
-                  >
+                    class="post__avatar">
                     <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
